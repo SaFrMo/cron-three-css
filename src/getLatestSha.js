3f199aa6e2e19e519c7cea0fa15f97b9f9a66e55
Sander <sander@funkhaus.us>
2019-01-17 15:37:41 -0500

    more bugfixes

--- src/getLatestSha.js
+++ src/getLatestSha.js
@@ -3,6 +3,7 @@ const _get = require('lodash/get')
 
 module.exports = async ({ user, repo, branch, path }) => {
     const url = `https://api.github.com/repos/${user}/${repo}/commits?sha=${branch}&path=${path}`
+    console.log(url)
 
     const json = await fetch(url).then(res => res.json())
     return _get(json, '[0].sha', null)
